8da99a10e3d7e7825404f5cac3937c30b68abeb8
Matthew Caravaggio <matthew.caravaggio@gmail.com>
2019-10-03 12:08:24 -0400

    Building card objects

---
+++ js/apps.js
@@ -0,0 +1,67 @@
+const deck = [
+    aceOfSpades = {
+        value: 1 || 11,
+        name: 'Ace of Spades',
+        img: src=""
+    },
+    twoOfSpades = {
+        value: 2,
+        name: 'Two of Spades',
+        img: src=""
+    },
+    threeOfSpades = {
+        value: 3,
+        name: 'Three of Spades',
+        img: src=""
+    },
+    fourOfSpades = {
+        value: 4,
+        name: 'Four of Spades',
+        img: src=""
+    },
+    fiveOfSpades = {
+        value: 5,
+        name: 'Five of Spades',
+        img: src=""
+    },
+    sixOfSpades = {
+        value: 6,
+        name: 'Six of Spades',
+        img: src=""
+    },
+    sevenOfSpades = {
+        value: ,
+        name: 'Ace of Spades',
+        img: src=""
+    },
+    aceOfSpades = {
+        value: 1 || 11,
+        name: 'Ace of Spades',
+        img: src=""
+    },
+    aceOfSpades = {
+        value: 1 || 11,
+        name: 'Ace of Spades',
+        img: src=""
+    },
+    aceOfSpades = {
+        value: 1 || 11,
+        name: 'Ace of Spades',
+        img: src=""
+    },
+    aceOfSpades = {
+        value: 1 || 11,
+        name: 'Ace of Spades',
+        img: src=""
+    },
+    aceOfSpades = {
+        value: 1 || 11,
+        name: 'Ace of Spades',
+        img: src=""
+    },
+    aceOfSpades = {
+        value: 1 || 11,
+        name: 'Ace of Spades',
+        img: src=""
+    },
+]
