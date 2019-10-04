07c8e7d7a55c7dd2bf179836c75a0316e40e6aa3
Matthew Caravaggio <matthew.caravaggio@gmail.com>
2019-10-04 10:35:16 -0400

    Shuffle function working. Thanks John!

--- js/apps.js
+++ js/apps.js
@@ -1,27 +1,30 @@
 // Using https://www.thatsoftwaredude.com/content/6196/coding-a-card-deck-in-javascript as a base
-
-const deck = new Array();
+// Deck creation
+const deck = [];
 const suits = ['spades', 'diamonds', 'clubs', 'hearts']
 const values = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'A', 'J', 'Q', 'K']
 
 const getDeck = () => {
-    let deck = new Array() 
     for(let i = 0; i < suits.length; i++) {
-        for(let x = 0; x < values.length; i++) {
-            let card = {Values: values[x], Suites: suits[i]}
+        for(let j = 0; j < values.length; j++) {
+            let card = {Values: values[j], Suites: suits[i]}
             deck.push(card)
+            //console.log(deck) 
         }
     }
-    return deck
+    // Checking to see if an unshuffled deck is generated
+    console.log(deck)
 }
 
-const shuffle = () => {
-    for(let i = 0; i < 1000; i++) {
+// Shuffling the deck
+const shuffle = (deck) => {
+    for(let i = 0; i < 100; i++) {
         let locationOne = Math.floor((Math.random() * deck.length))
         let locationTwo = Math.floor((Math.random() * deck.length))
         let tmp = deck[locationOne]
-
         deck[locationOne] = deck[locationTwo]
         deck[locationTwo] = tmp
     }
+    console.log(deck)
 }
+
